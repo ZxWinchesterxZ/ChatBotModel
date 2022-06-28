8893bd6b2fcbc1ea76cf99fb8f6e4bee376abc5b
ZxWinchesterxZ <89107179+ZxWinchesterxZ@users.noreply.github.com>
2022-06-28 20:17:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-app = FastAPI()
 from joblib import load as load_j
 from nltk.stem import WordNetLemmatizer
+import uvicorn
+app = FastAPI()
+port = os.environ["PORT"]
 def lem(query,lemmatizer):
     return lemmatizer.lemmatize(query)
 @app.get("/")
@@ -14,5 +16,8 @@ def predict(qu:str):
     qu= lem(qu,lemmatizer)
     Pipe2=load_j('withoutTreatment_withSymp3.pkl')
     return str(Pipe2.predict([qu])[0])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
 
 
